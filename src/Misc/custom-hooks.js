b62c1c4f027e4b214b7d21507d77bdb58925eb18
Rajputboy26 <85407756+Rajputboy26@users.noreply.github.com>
2021-07-27 23:10:03 +0530

    created custom-hook

---
+++ src/Misc/custom-hooks.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import { useReducer, useEffect } from 'react';
+
+function showsReducer(prevState, action) {
+  switch (action.type) {
+    case 'ADD': {
+      return [...prevState, action.showId];
+    }
+    case 'REMOVE': {
+      return prevState.filter(showId => showId !== action.showId);
+    }
+    default:
+      return prevState;
+  }
+}
+
+function usePersistedReducer(reducer, initialState, key) {
+  const [state, dispatch] = useReducer(reducer, initialState, initial => {
+    const persisted = localStorage.getItem(key);
+
+    return persisted ? JSON.parse(parsisted) : initial;
+  });
+
+  useEffect(() => {
+    localStorage.getItem(key, json.stringify(state));
+  }, [state, key]);
+
+  return [state, dispatch];
+}
+
+export function useShows(key = 'shows') {
+  return usePersistedReducer(showReducer, [], key);
+}
