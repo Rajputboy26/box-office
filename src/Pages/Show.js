abc7749546483dcad34216cb33f1f7a12d746f98
Rajputboy26 <85407756+Rajputboy26@users.noreply.github.com>
2021-07-27 13:44:41 +0530

    added readmore page

---
+++ src/Pages/Show.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiGet } from '../Misc/Config';
+
+const Show = () => {
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+
+  useEffect(() => {
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
+      setShow(results);
+    });
+  }, [id]);
+  console.log(show);
+
+  return <div>this is a show page</div>;
+};
+
+export default Show;
