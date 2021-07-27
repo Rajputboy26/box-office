de20337a2f2217a0671b043b53d04077d1430683
Rajputboy26 <85407756+Rajputboy26@users.noreply.github.com>
2021-07-27 16:37:20 +0530

    loading && error msg added

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-const */
+/* eslint-disable consistent-return */
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../Misc/Config';
@@ -5,15 +8,40 @@ import { apiGet } from '../Misc/Config';
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
-  }, [id]);
-  console.log(show);
+    let isMounted = true;
 
-  return <div>this is a show page</div>;
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        if (isMounted) {
+          setShow(results);
+          setIsLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setIsLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [id]);
+
+  //   console.log(show);
+
+  if (isLoading) {
+    return <div>data is loading</div>;
+  }
+  if (error) {
+    return <div>Error occured:{error}</div>;
+  }
+  return <div>hi surya</div>;
 };
 
 export default Show;
